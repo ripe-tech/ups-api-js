d24a0b85bff3fbc4c0b236dc13f0946fd89350c3
João Magalhães <joamag@gmail.com>
2021-05-18 22:47:46 +0100

    fix: unit tests

--- test/api/base.js
+++ test/api/base.js
@@ -4,7 +4,7 @@ const ups = require("../..");
 describe("API", function() {
     it("should be able to instantiate the API", async () => {
         const api = new ups.API();
-        assert.strictEqual(Boolean(api.UPS_SHIPPING_BASE_URL), true);
-        assert.strictEqual(Boolean(api.UPS_DOCUMENT_BASE_URL), true);
+        assert.strictEqual(Boolean(api.shippingBaseUrl), true);
+        assert.strictEqual(Boolean(api.documentBaseUrl), true);
     });
 });
