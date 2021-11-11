604c6db571910c6d5eb7d2b2b763efc5dd6eb2c6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-11-11 12:18:18 +0000

    feat: xml based tracking API

--- js/tracking.js
+++ js/tracking.js
@@ -1,3 +1,5 @@
+import { getXMLHeader, xmlToJson } from "./util";
+
 export const TrackingAPI = superclass =>
     class extends superclass {
         getTrackingUrl(trackingNumber) {
@@ -5,11 +7,29 @@ export const TrackingAPI = superclass =>
         }
 
         async getTrackingDetails(trackingNumber, options = {}) {
-            const url = `${this.trackingBaseUrl}details/${trackingNumber}`;
-            const response = await this.get(url, {
+            const xmlRequest = this._buildTrackingRequest(trackingNumber);
+            const response = await this.post(this.trackingBaseUrl, {
                 kwargs: { auth: "headers" },
+                mime: "application/xml",
+                data: xmlRequest,
                 ...options
             });
-            return response;
+            const xmlResponse = await response.text();
+            const jsonResponse = xmlToJson(xmlResponse);
+            return jsonResponse;
+        }
+
+        _buildTrackingRequest(trackingNumber) {
+            const xmlRequest =
+                getXMLHeader(this.username, this.password, this.license) +
+                `<?xml version="1.0"?>
+                <TrackRequest>
+                    <Request>
+                        <RequestAction>Track</RequestAction>
+                        <RequestOption>8</RequestOption>
+                    </Request>
+                    <TrackingNumber>${trackingNumber}</TrackingNumber>
+                </TrackRequest>`;
+            return xmlRequest;
         }
     };
