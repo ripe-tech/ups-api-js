0131029cf835fbef3fde8ebca91d1ad0a3d30192
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-11-07 16:17:59 +0000

    fix: tracking API

--- js/tracking.js
+++ js/tracking.js
@@ -31,17 +31,6 @@ export const STATUS_RETURNED = "RS";
 export const TrackingAPI = superclass =>
     class extends superclass {
         /**
-         * Gets the tracking URL given a tracking number.
-         *
-         * @param {String} trackingNumber The tracking number of the shipment/waybill.
-         * @returns {String} The tracking URL.
-         * @see https://www.ups.com/upsdeveloperkit?loc=en_US
-         */
-        getTrackingUrl(trackingNumber) {
-            return `http://wwwapps.ups.com/WebTracking/processInputRequest?TypeOfInquiryNumber=T&InquiryNumber1=${trackingNumber}`;
-        }
-
-        /**
          * Gets the tracking information for an existing shipment.
          *
          * @param {String} trackingNumber The tracking number of the shipment/waybill.
@@ -50,11 +39,8 @@ export const TrackingAPI = superclass =>
          * @see https://www.ups.com/upsdeveloperkit?loc=en_US
          */
         async getTrackingDetails(trackingNumber, options = {}) {
-            const url = `${this.trackingBaseUrl}details/${trackingNumber}`;
-            const response = await this.get(url, {
-                kwargs: { auth: "headers" },
-                ...options
-            });
+            const url = `${this.baseUrl}track/${this.version}/details/${trackingNumber}`;
+            const response = await this.get(url, options);
             return response;
         }
 
@@ -69,29 +55,14 @@ export const TrackingAPI = superclass =>
          * @see https://www.ups.com/upsdeveloperkit?loc=en_US
          */
         async getTrackingDetailsExtended(trackingNumber, options = {}) {
-            const data = this._buildTrackingPayload(trackingNumber);
-            const response = await this.post(this._getTrackingXmlBaseUrl(), {
-                kwargs: { auth: "headers" },
-                mime: "application/xml",
-                data: data,
-                ...options
-            });
-            const xml = await response.text();
-            const result = xmlToJson(xml);
-            return result;
-        }
-
-        _buildTrackingPayload(trackingNumber) {
-            const xmlRequest =
-                getXMLHeader(this.username, this.password, this.license) +
-                `<?xml version="1.0"?>
-                <TrackRequest>
-                    <Request>
-                        <RequestAction>Track</RequestAction>
-                        <RequestOption>9</RequestOption>
-                    </Request>
-                    <TrackingNumber>${trackingNumber}</TrackingNumber>
-                </TrackRequest>`;
-            return xmlRequest;
+            return {
+                TrackResponse: {
+                    Response: {
+                        Error: {
+                            Message: "Not Implemented By UPS API Client"
+                        }
+                    }
+                }
+            };
         }
     };
